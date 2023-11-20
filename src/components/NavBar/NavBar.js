885d59094a9f9ae52415ecc04d6ee49ee0cc23b1
Mohamed Elselhab <momanchester1992@gmail.com>
2023-11-20 17:08:21 +0000

    update the navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -48,52 +48,110 @@
 
 // export default NavBar;
 
-import "./NavBar.css";
-import logo from "../../Assets/logo.png";
-import { BsCart3 } from "react-icons/bs";
 import { useState } from "react";
+import { BsCart3 } from "react-icons/bs";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { FaTimes } from "react-icons/fa";
 import { Link } from "react-router-dom";
+import logo from "../../Assets/logo.png";
+import "./NavBar.css";
+import { Buttons } from "../index";
 
-const NavBar = () => {
+const Navbar = () => {
+  const [click, setClick] = useState(false);
   const [menu, setMenu] = useState("shop");
 
+  const handleClick = () => setClick(!click);
+
+  const closeMenu = () => setClick(false);
+
   return (
-    <header>
-      <div className="header">
+    <header className="header">
+      <nav className="navbar">
+        {/* logo */}
         <div className="logo">
-          <img src={logo} alt="logo" />
-          <span>shopper</span>
+          <Link to="/">
+            <img src={logo} alt="logo" />
+            <span>shopper</span>
+          </Link>
         </div>
-        <nav>
-          <ul className="nav-menu">
-            <li className="nav-item" onClick={() => setMenu("shop")}>
-              <Link to="/">shop</Link>
-              {menu === "shop" ? <hr /> : <></>}
-            </li>
-            <li className="nav-item" onClick={() => setMenu("women")}>
-              <Link to="/women">Women</Link>
-              {menu === "women" ? <hr /> : <></>}
-            </li>
-            <li className="nav-item" onClick={() => setMenu("men")}>
-              <Link to="/men">Men</Link>
-              {menu === "men" ? <hr /> : <></>}
-            </li>
-            <li className="nav-item" onClick={() => setMenu("kids")}>
-              <Link to="/kids">Kids</Link>
-              {menu === "kids" ? <hr /> : <></>}
-            </li>
-          </ul>
-        </nav>
-        <div className="login">
-          <button>Login</button>
+        {/* desktop links */}
+        <ul className="desktop-links">
+          <li className="nav-item" onClick={() => setMenu("shop")}>
+            <Link to="/">shop</Link>
+            {menu === "shop" ? <hr /> : <></>}
+          </li>
+          <li className="nav-item" onClick={() => setMenu("women")}>
+            <Link to="/women">Women</Link>
+            {menu === "women" ? <hr /> : <></>}
+          </li>
+          <li className="nav-item" onClick={() => setMenu("men")}>
+            <Link to="/men">Men</Link>
+            {menu === "men" ? <hr /> : <></>}
+          </li>
+          <li className="nav-item" onClick={() => setMenu("kids")}>
+            <Link to="/kids">Kids</Link>
+            {menu === "kids" ? <hr /> : <></>}
+          </li>
+        </ul>
+        {/* mobile links */}
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <div className="login">
+              <Link to="/cart" onClick={closeMenu}>
+                your cart
+                <BsCart3 className="cart-icon" />
+              </Link>
+              <div className="cart-count">0</div>
+            </div>
+          </li>
+
+          <li className="nav-item">
+            <Link to="/" onClick={closeMenu}>
+              Shop
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/women" onClick={closeMenu}>
+              women
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/men" onClick={closeMenu}>
+              men
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/kids" onClick={closeMenu}>
+              kids
+            </Link>
+          </li>
+        </ul>
+        <div className="login login-cart">
+          <Buttons>Login</Buttons>
           <Link to="/cart">
             <BsCart3 className="cart-icon" />
           </Link>
           <div className="cart-count">0</div>
         </div>
-      </div>
+        <div className="hamburger" onClick={handleClick}>
+          {click ? (
+            <FaTimes
+              size={30}
+              style={{ color: "#000" }}
+              className="menu-icon"
+            />
+          ) : (
+            <GiHamburgerMenu
+              size={30}
+              style={{ color: "#000" }}
+              className="menu-icon"
+            />
+          )}
+        </div>
+      </nav>
     </header>
   );
 };
 
-export default NavBar;
+export default Navbar;
